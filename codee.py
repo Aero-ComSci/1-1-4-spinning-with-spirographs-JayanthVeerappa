72d29d30647f3ad2175e224ac894cd47173c9f1b
JayanthVeerappa <132962391+JayanthVeerappa@users.noreply.github.com>
2024-09-10 13:51:12 -0700

    Update codee.py

--- codee.py
+++ codee.py
@@ -1,28 +1,31 @@
 import turtle as trtl
+import random
+
+painter = trtl.Turtle()
 
 screen = trtl.Screen()
 screen.setup(width=800, height=800)
 
-trtl = trtl.Turtle()
-trtl.speed(0)
+painter.speed(0)
 
 colors = ['red', 'green', 'blue', 'black', 'orange', 'purple', 'pink', 'brown']
-
 size = 20
-size_incrm = 20
-incrm = 0
+size_incrm = 10 
 
-while True:  
-    trtl.penup()
-    trtl.goto(-size / 2, -size / 2)
-    trtl.pendown()
-    trtl.color(colors[incrm % len(colors)]) 
-    j = 0
-    while j < 4:  
-        trtl.forward(size)
-        trtl.left(90)
-        j += 1
-    size += size_incrm 
-    incrm += 1
+for _ in range(50):  
+    painter.penup()
+    painter.goto(-size / 2, -size / 2)  
+    painter.pendown()
     
+    color = random.choice(colors)
+    painter.color(color) 
+    
+    painter.color()
+    for _ in range(4):
+        painter.forward(size)
+        painter.left(90)
+
+    
+    size += size_incrm
+
 screen.mainloop()
