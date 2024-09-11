784660cb007d97fd5fda88a46275fdc05aa244f4
JayanthVeerappa <132962391+JayanthVeerappa@users.noreply.github.com>
2024-09-11 14:15:41 -0700

    Update Zerio itteration loops and infinite loops.py

--- Zerio itteration loops and infinite loops.py
+++ Zerio itteration loops and infinite loops.py
@@ -1,1 +1,33 @@
+import turtle as trtl
 
+painter = trtl.Turtle()
+painter.speed(0)
+colors = ['red', 'green', 'blue', 'black', 'orange', 'purple', 'pink', 'brown']
+size = 20
+size_incrm = 10 
+go = True
+while go == False:  # this is a Zero iteration loop, it will never run because go is set to True, but it needs go to be false to run
+#This is also an infinite loop, because it will run forever  as long as go is false 
+    
+
+    for _ in range(50):  
+        painter.penup()
+        painter.goto(-size / 2, -size / 2)  
+        painter.pendown()
+        
+        color = random.choice(colors)
+        painter.color(color) 
+        
+        painter.color()
+        for _ in range(4):
+            painter.forward(size)
+            painter.left(90)
+
+        
+        size += size_incrm
+
+screen.mainloop()
+
+
+wn = trtl.Screen()
+wn.mainloop()
